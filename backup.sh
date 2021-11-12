47560a1cb85619fc55a574fb2057e9c995353380
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-11-12 13:36:47 -0300

    Updates

--- backup.sh
+++ backup.sh
@@ -9,25 +9,6 @@ mkdir -p .config/zsh/
 cp $ZDOTDIR/{.zshrc,.zlogin,.zshvi,.zshAliasFunrc} .config/zsh/
 cp $HOME/.zshenv .
 
-# KDE Plasma
-cp $XDG_CONFIG_HOME/{breezerc,kdeglobals,kglobalshortcutsrc,khotkeysrc,klipperrc,krunnerrc,kwinrc,plasma-org.kde.plasma.desktop-appletsrc,plasmanotifyrc} .config
-cp $XDG_CONFIG_HOME/{okularpartrc,okularrc,powerdevilrc,powermanagementprofilesrc,startkderc} .config
-
-# breezerc
-# kdeglobals
-# kglobalshortcutsrc
-# khotkeysrc
-# klipperrc
-# krunnerrc
-# kwinrc
-# okularpartrc
-# okularrc
-# plasma-org.kde.plasma.desktop-appletsrc
-# plasmanotifyrc
-# powerdevilrc
-# powermanagementprofilesrc
-# startkderc
-
 mkdir -p .local/share/
 cp -r $HOME/.local/share/color-schemes .local/share
 
