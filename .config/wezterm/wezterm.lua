62bae673ac5b3148ff37fba133701f021c585802
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-03-08 20:50:45 -0300

    Added rose-pine moon colorscheme for wezterm and plasma

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,55 +1,131 @@
 local wezterm = require('wezterm')
 
 return {
+   -- Fonts
+   -- font = wezterm.font('Operator Mono'),
+   -- font = wezterm.font('Cartograph CF'),
+   font = wezterm.font('JetBrains Mono'),
+   -- font = wezterm.font('Iosevka'),
+   -- font_size = 14,
+   font_size = 14,
+   -- font_size = 13.6,
+   -- line_height = 1.2,
+   -- line_height = 1.05,
+   line_height = 1.14,
 
-    -- Fonts
-    font = wezterm.font('Iosevka Term'),
-    -- font_size = 14,
-    font_size = 15.0,
-    line_height = 1.08,
+   custom_block_glyphs = false,
+   foreground_text_hsb = {
+      hue = 1.0,
+      saturation = 1.0,
+      brightness = 1.05,
+   },
+   window_padding = {
+      top = 0,
+      left = 0,
+      right = 0,
+      bottom = 0,
+   },
 
-    custom_block_glyphs = false,
-    -- foreground_text_hsb = {
-    --     hue = 1.0,
-    --     saturation = 1.00,
-    --     brightness = 1.01,
-    -- },
-    hide_tab_bar_if_only_one_tab = true,
-    window_padding = {
-        top = 0,
-        left = 0,
-        right = 0,
-        bottom = 0,
-    },
-    -- window_background_opacity = 0.96,
-    -- text_background_opacity = 0.9,
-    use_dead_keys = false, -- For áéíóú
-    enable_wayland = true,
-    colors = {
-        foreground = '#716D8B', -- The default text color
-        background = '#191724', -- The default background color
+   -- window_background_opacity = 0.9,
+   -- text_background_opacity = 0.9,
 
-        cursor_bg = '#555169',
-        cursor_fg = '#e0def4',
-        -- cursor_border = 'orange',
+   use_dead_keys = true, -- For áéíóú
+   enable_wayland = true,
 
-        selection_fg = 'black', -- the foreground color of selected text
-        selection_bg = 'orange', -- the background color of selected text
+   -- Tab bar
+   hide_tab_bar_if_only_one_tab = true,
+   enable_tab_bar = true,
+   use_fancy_tab_bar = false,
+   tab_max_width = 33,
+   tab_bar_at_bottom = true,
 
-        split = '#3f3c53', -- The color of the split lines between panes
+   colors = {
 
-        ansi = { '#26233A', '#b4637a', '#286983', '#D4A76C', '#9CCFD8', '#C4A7E7', '#D7827E', '#E0DEF4' },
-        brights = { '#3B425B', '#EB6F92', '#31748F', '#F6C177', '#9CCFD8', '#C4A7E7', '#D7827E', '#E0DEF4' },
-        indexed = { [136] = '#af8700' }, -- Arbitrary colors of the palette in the range from 16 to 255
-        -- indexed = {[136] = '#716D8B'}, -- Arbitrary colors of the palette in the range from 16 to 255
-    },
-    force_reverse_video_cursor = true,
+      indexed = { [136] = '#af8700' }, -- Arbitrary colors of the palette in the range from 16 to 255
 
-    window_close_confirmation = 'NeverPrompt',
-    exit_behavior = 'Close',
-    skip_close_confirmation_for_processes_named = {
-        'zsh',
-        'nvim',
-    },
-    check_for_updates = false,
+      -- Rosé Pine dark, ansi colors are darken colors from the main palette
+      -- foreground = '#716D8B', -- The default text color
+      -- background = '#191724', -- The default background color
+      -- cursor_bg = '#555169',
+      -- cursor_fg = '#e0def4',
+      -- cursor_border = 'orange',
+      -- selection_fg = 'black', -- the foreground color of selected text
+      -- selection_bg = 'orange', -- the background color of selected text
+      -- split = '#3f3c53', -- The color of the split lines between panes
+      -- ansi = {
+      --    '#26233a',
+      --    '#b4637a',
+      --    '#286983',
+      --    '#d4a76c',
+      --    '#9ccfd8',
+      --    '#c4a7e7',
+      --    '#d7827e',
+      --    '#e0def4'
+      -- },
+      -- brights = {
+      --    '#3b425b',
+      --    '#eb6f92',
+      --    '#31748f',
+      --    '#f6c177',
+      --    '#9ccfd8',
+      --    '#c4a7e7',
+      --    '#d7827e',
+      --    '#e0def4'
+      -- },
+
+      -- Rosé Pine moon
+      foreground = '#908caa', -- The default text color
+      background = '#232136', -- The default background color
+      cursor_bg = '#56526e',
+      cursor_fg = '#e0def4',
+      selection_fg = 'black', -- the foreground color of selected text
+      selection_bg = '#3e8fb0', -- the background color of selected text
+      split = '#2a273f', -- The color of the split lines between panes
+      tab_bar = {
+         background = '#232136',
+         active_tab = {
+            bg_color = '#2a273f',
+            fg_color = '#e0def4',
+         },
+         inactive_tab = {
+            bg_color = '#191724',
+            fg_color = '#6e6a86',
+         },
+         new_tab = {
+            bg_color = '#232136',
+            fg_color = '#6e6a86',
+         },
+         new_tab_hover = {
+            bg_color = '#2a273f',
+            fg_color = '#6e6a86',
+            italic = true,
+         },
+      },
+      ansi = {
+         '#2a273f',
+         '#b4637a',
+         '#286983',
+         '#d4a76c',
+         '#9ccfd8',
+         '#c4a7e7',
+         '#d7827e',
+         '#e0def4',
+      },
+      brights = {
+         -- '#393552',
+         '#575279',
+         '#eb6f92',
+         '#31748f',
+         '#f6c177',
+         '#9ccfd8',
+         '#c4a7e7',
+         '#d7827e',
+         '#e0def4',
+      },
+   },
+   force_reverse_video_cursor = true,
+   window_close_confirmation = 'NeverPrompt',
+   exit_behavior = 'Close',
+   skip_close_confirmation_for_processes_named = { 'zsh', 'nvim' },
+   check_for_updates = false,
 }
